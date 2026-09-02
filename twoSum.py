3c8119387b64ff8a86ac8c7f9d2646ceac09d566
SanuSamanta-oss <changeislaw777@gmail.com>
2026-09-02 22:00:27 +0530

    Two sum problem solution

---
+++ twoSum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self,nums,target):
+        output = []
+        for i in range(len(nums)):
+            for j in range(i):
+                if ((nums[i] + nums[j]) == target):
+                    output.append(j)
+                    output.append(i)
+                    return output
+
+nums = [2, 7, 21, 4]
+target = 9
+solution = Solution()
+print(solution.twoSum(nums,target))
+
