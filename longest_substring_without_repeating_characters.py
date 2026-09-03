075096d66ac04563b522bb1ed0997d4ecfd188ba
SanuSamanta-oss <changeislaw777@gmail.com>
2026-09-03 21:15:56 +0530

    find the length of the longest substring

---
+++ longest_substring_without_repeating_characters.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthLongestofSubstring(self, s):
+        seen = set()
+        left = 0
+        maximum = 0
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[right])
+
+            maximum = max(maximum , right - left + 1)
+
+        return maximum
+
+str = input("Enter a string (with or without duplicagte characters): ")
+
+obj = Solution()
+print("Longest Substring length : ", obj.lengthLongestofSubstring(str))
