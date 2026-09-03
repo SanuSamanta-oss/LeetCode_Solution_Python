ccdc0c4bc7cafcf155e47532da549a3f88d7d447
SanuSamanta-oss <changeislaw777@gmail.com>
2026-09-03 21:58:08 +0530

    Median of two sorted array

---
+++ Median_of_two_sorted_array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findMedianSortedArray(self , nums1 , nums2):
+
+        arr = nums1 + nums2
+        arr.sort()
+        n = len(arr)
+
+        if (n % 2) == 1:
+            return n // 2
+        else:
+            mid1 = arr[n // 2 - 1]
+            mid2 = arr[n // 2]
+
+            return (mid1 + mid2) / 2.0
+
+
+
+nums1 = list(map(int , input("Enter the elements of number one array: ").split()))
+nums2 = list(map(int , input("Enter the elements of number two array: ").split()))
+
+obj = Solution()
+answer = obj.findMedianSortedArray(nums1, nums2)
+print("Median of two sorted array: ",answer)
