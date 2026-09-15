866b94b1998f892aaf859777365449c18f568257
SanuSamanta-oss <changeislaw777@gmail.com>
2026-09-15 22:59:28 +0530

    Valid parentheses problem

---
+++ valid_parentheses.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>(); // empty Stack created and "stack" is the name of the variable Stack<Character> means  it will be character type 
+
+        for ( char c : s.toCharArray()){
+            if ( c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            else{
+                if( stack.isEmpty()){
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (c == ')' && top != '('){
+                    return false;
+                }
+                if (c == '}' && top != '{'){
+                    return false;
+                }
+                if (c == ']' && top != '['){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
